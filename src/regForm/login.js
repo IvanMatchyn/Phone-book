eff26c9a535a036c2b5a2268e77c5095aa79395a
winter4nes <winter.nes@gmail.com>
2019-01-05 12:52:09 +0200

    reg form started

--- src/regForm/login.js
+++ src/regForm/login.js
@@ -1,7 +1,10 @@
 (function() {
-    fetch('../src/regForm/login.html')
-        .then(function (response) {
-            console.log(response)
+    const regForm = document.querySelector('.ls__log-reg-form')
+
+    fetch('../src/regForm/login.html').then(function (response) {
+            return response.text().then(function(text) {
+                regForm.innerHTML = text;
+            });
         })
 })();
 
