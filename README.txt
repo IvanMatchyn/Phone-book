cdaea2396a8c02b8f55ec56a8e1e5b1d308ae52a
winter4nes <winter.nes@gmail.com>
2019-02-25 15:14:08 +0200

    Complete. V. 1.2.2 Rework file Tree && Some Fixes

--- README.txt
+++ README.txt
@@ -2,5 +2,5 @@ For launch project you need:
 
 1.Install Node.js
 2.Use Command Line to choose folder with project
-3.1 Type 'npm run build' to build project files.
-3.2 Type  'npm run start' to run webpack dev server with this project
+3.Type 'npm run build' to build project files.
+4.Type 'npm run start' to run webpack dev server with this project
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
+import './media.scss'
 import './index.scss'
 import './normalize.css'
-import './fonts/fonts.css'
+import '../assets/fonts/fonts.css'
 import './Constants.js'
-import  './Module.js'
-import './Constants.js'
+import './Module.js'
+import './AllContacts/all-contacts-page-style.scss'
+import './Categories/create-category-styles.scss'
+import './ContactInfo/contact-info-styles.scss'
+import './Registration/registration-page-styles.scss'
+import './EditContact/edit-contact-page-styles.scss'
 import './LoginPage/LoginPage.js'
+import './LoginPage/login-page-styles.scss'
 import './AddContact/AddContact.js'
+import './AddContact/add-page-styles.scss'
 import './HomePage/HomePage.js'
+import './HomePage/home-page-style.scss'
 
 import ContactsBook from "./Module.js";
 
